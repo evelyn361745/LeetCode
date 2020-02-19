a60de6c3bf7abafc7f88df6597f53b50d5b4c754
evelyn361745 <44867428+evelyn361745@users.noreply.github.com>
2020-02-19 14:54:19 +0800

    Update 283.移动零.js
    双指针优化解法

--- code/283.移动零.js
+++ code/283.移动零.js
@@ -18,3 +18,18 @@ var moveZeroes = function(nums) {
     }
     return nums;
 };
+
+//双指针优化解法
+var moveZeroes = function(nums) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== 0) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    for (; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+    return nums;
+};
