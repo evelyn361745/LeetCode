d082702f050a18fd7b9958062d6513c12f512bb0
evelyn361745 <1139447839@qq.com>
2020-02-21 16:09:57 +0800

    替换空格.js

---
+++ 剑指offer(javascript)/字符串/替换空格.js
@@ -0,0 +1,16 @@
+/**
+ * 题目描述
+* 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+* 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ */
+
+function replaceSpace(str)
+{
+    // write code here
+    let strarr = str.split(" ");
+    return strarr.join("%20")
+}
+
+
+let str = "We Are Happy"
+console.log(replaceSpace(str))
